723758f8ab73bfa8f2eca50fc8ed23bbe338e799
bon9 <rnem8831@gmail.com>
2024-08-30 13:01:09 +0900

    aug30

---
+++ algorithm/src/aug30.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class aug30 {
+    public static void main(String[] args) {
+        aug30 aug30 = new aug30();
+        System.out.println(aug30.solution(new int[]{0,0}));
+    }
+
+    public String solution(int[] numbers) {
+        List<String> str = new ArrayList<>();
+        for (int number : numbers){
+            str.add(number+"");
+        }
+        str.sort((o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        String answer = String.join("",str);
+        System.out.println(answer.substring(0));
+        return answer.substring(0,1).equals("0") ? "0" : answer;
+    }
+
+}
